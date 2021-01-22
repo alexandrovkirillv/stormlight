eeddcf2bab8130c8558057e2db54ffa925a2e213
Alex <alexandrovkirillv@mail.ru>
2021-01-23 03:16:32 +0700

    Added time column

--- src/main/java/com/secretNet/secNet/models/Post.java
+++ src/main/java/com/secretNet/secNet/models/Post.java
@@ -14,6 +14,7 @@ public class Post {
     private String title;
     private String anons;
     private String full_text;
+    @Column(nullable=false)
     private String time;
     private int views;
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss z");
