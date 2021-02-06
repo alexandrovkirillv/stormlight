982650161036ab8663010c06505a89172402b806
Alex <alexandrovkirillv@mail.ru>
2021-02-07 00:05:36 +0700

    solve null on auth

--- src/main/java/com/secretNet/secNet/config/WebConfiguration.java
+++ src/main/java/com/secretNet/secNet/config/WebConfiguration.java
@@ -1,8 +1,12 @@
 package com.secretNet.secNet.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurationSupport {
@@ -11,5 +15,10 @@ public class WebConfiguration extends WebMvcConfigurationSupport {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/images/");
+    }
+
+    @Override
+    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new AuthenticationPrincipalArgumentResolver());
     }
 }
