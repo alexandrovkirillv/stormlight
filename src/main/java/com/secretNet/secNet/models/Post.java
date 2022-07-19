e100439db0f491d45ac13d3d0018312f971eb752
kirillalexandrov <alexandrovkirillv@mail.ru>
2022-07-19 14:04:06 +0300

    first again

--- src/main/java/com/secretNet/secNet/models/Post.java
+++ src/main/java/com/secretNet/secNet/models/Post.java
@@ -12,7 +12,6 @@ import java.util.TimeZone;
 @Data
 @EntityScan
 public class Post implements Comparable<Post> {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
